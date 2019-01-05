9196525c3c2412cef9c172da80aa7f73ff24e45a
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-05 12:04:13 -0200

    Impl: a Listagem de Tarefas no: Componete e na View(pagina)

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TarefaService, Tarefa } from '../shared';
+
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa []; /*armazena as tarefas */
+
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit() {
+    this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1, " Tarefa 1", false),
+      new Tarefa(2, " Tarefa 2", true),
+    ];
+
   }
 
+/* lsitar Todas - pega os dados do service e envia para o html */
+listarTodos(): Tarefa [] {
+  return this.tarefaService.listarTodos();
+}
 }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; /*import de modulerotas*/
+import { FormsModule } from '@angular/forms';  /*import modelule de formualrio*/
 
 import { TarefaService} from './shared';
 import { ListarTarefaComponent } from './listar';

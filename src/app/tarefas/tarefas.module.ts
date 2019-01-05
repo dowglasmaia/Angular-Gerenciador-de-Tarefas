437d510e65094ac5b6b181ef81bac9b297ef28cb
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-05 12:16:39 -0200

    Criando o componente de cadastro e fazendo as configurações

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';  /*import modelule de formualrio*/
 
 import { TarefaService} from './shared';
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 @NgModule({
-  declarations: [ListarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent],
   imports: [
     CommonModule
   ],
